cd32b0c2377f4e58ee0c48077126a067a14fee7e
VA_24 <24agnihotriv@smtexas.org>
2023-12-31 20:45:30 -0600

    updates to multiplayer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import ScibowlScrimHome from './pages/ScibowlScrimHome';
 import ScibowlScrimSingleplayer from './pages/ScibowlScrimSingleplayer';
+import ScibowlScrimMultiplayer from './pages/ScibowlScrimMultiplayer';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Routes>
       <Route exact path='/' element={<ScibowlScrimHome />}/>
-        <Route exact path='/singleplayer' element={<ScibowlScrimSingleplayer />}/>
+      <Route exact path='/singleplayer' element={<ScibowlScrimSingleplayer />}/>
+      <Route exact path='/multiplayer' element={<ScibowlScrimMultiplayer />}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/ScibowlScrimMultiplayer.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect} from 'react';
+import '../App.css';
+
+function ScibowlScrimMultiplayer(){
+    return(
+        
+        <body class='bg-white min-h-screen m-0'>
+            <div class='relative flex flex-col items-center'>
+            <main class='w-full flex flex-col lg:flex-row-reverse sticky top-0 px-6 pt-3 py-3 bg-blue-200 z-[2] mb-6'>
+            <nav class='w-full flex flex-row items-center justify-center gap-20 tracking-tight'>
+                <a href='/'>
+                <button class='text-xl xl:text-lg flex items-center text-black hover:text-white ease-linear transition'>
+                    Home
+                </button>
+                </a>
+
+                <a href='/singleplayer'>
+                <button class='text-xl xl:text-lg flex items-center text-black hover:text-white ease-linear transition'>
+                    Singleplayer
+                </button>
+                </a>
+
+                <a href='/multiplayer'>
+                <button class='text-xl xl:text-lg flex items-center text-black hover:text-gray-400 ease-linear transition'>
+                    Multiplayer
+                </button>
+                </a>
+                </nav>
+            </main>
+
+            <section class='relative flex flex-col justify-center items-center w-2/3'>
+
+                <h3 class='text-xl xl:text-2xl font-medium text-black mb-8'>
+                    Coming Soon!
+                </h3>
+
+            </section>
+                
+            </div>
+        
+        </body>
+    )
+}
+
+export default ScibowlScrimMultiplayer;
